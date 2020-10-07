113f47bd15f7c71f1bd1893f718ced2bd6544252
ismael <ismaelom83@gmail.es>
2020-10-07 15:00:42 +0200

    deploy heroku

--- proyecto_tienda/src/main/java/com/proyecto_tienda/controller/ClienteController.java
+++ proyecto_tienda/src/main/java/com/proyecto_tienda/controller/ClienteController.java
@@ -545,7 +545,6 @@ public class ClienteController {
 			Optional<Persona> personaNueva = personaRepoInterface.findBymail(persona.getMail());
 
 			if (!personaNueva.isPresent()) {
-				modelo.addAttribute("persona", persona);
 				traSer.registrarPersona(persona);
 				persona = cliService.consultaUltimoCliente();
 				cliService.registrarClientes(persona.getId(), 10000, 10000, "normal");
