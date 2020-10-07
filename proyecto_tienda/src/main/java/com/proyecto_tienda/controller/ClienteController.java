b1a2921651533f47e007d53af66ec60cc935318a
ismael <ismaelom83@gmail.es>
2020-10-07 18:32:11 +0200

    deploy heroku

--- proyecto_tienda/src/main/java/com/proyecto_tienda/controller/ClienteController.java
+++ proyecto_tienda/src/main/java/com/proyecto_tienda/controller/ClienteController.java
@@ -546,7 +546,7 @@ public class ClienteController {
 
 			if (!personaNueva.isPresent()) {
 				try {
-					traSer.registrarPersona(persona);
+				persona =	traSer.registrarPersona(persona);
 					persona = cliService.consultaUltimoCliente();
 					cliService.registrarClientes(persona.getId(), 10000, 10000, "normal");
 				} catch (Exception e) {
